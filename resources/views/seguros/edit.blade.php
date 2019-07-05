20b6ba51d83a43f98dfee9bd1a02ceb599e562c6
Riko <f.bunout01@ufromail.cl>
2019-07-04 23:09:49 -0400

    crud Seguro

--- resources/views/seguros/edit.blade.php
+++ resources/views/seguros/edit.blade.php
@@ -19,7 +19,25 @@
             </ul>
         </div>
     @endif
-  
+
+
+    <form>
+        <div class="form-group row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{ $seguro->patente }}" name="patente">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            </div>
+        </div>
+    </form>
+
+
+
     <form action="{{ route('seguro.update',$seguro->patente) }}" method="POST">
         @csrf
         @method('PUT')
