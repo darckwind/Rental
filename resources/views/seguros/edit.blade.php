923a5142096fbcbe67a5379c12217d29d9612c57
Riko <f.bunout01@ufromail.cl>
2019-07-04 23:24:08 -0400

    crud Seguro

--- resources/views/seguros/edit.blade.php
+++ resources/views/seguros/edit.blade.php
@@ -24,18 +24,21 @@
     <form>
         <div class="form-group row">
             <label for="staticEmail" class="col-sm-2 col-form-label">Patente</label>
-            <div class="col-sm-6">
+            <div class="col-sm-4">
                 <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{ $seguro->patente }}" name="patente">
             </div>
             <label for="staticEmail" class="col-sm-2 col-form-label">Poliza</label>
-            <div class="col-sm-6">
+            <div class="col-sm-4">
                 <input type="text" class="form-control-plaintext" id="staticEmail" value="{{ $seguro->poliza }}" name="patente">
             </div>
         </div>
         <div class="form-group row">
             <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                <label for="staticEmail" class="col-sm-2 col-form-label">Patente</label>
+                <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{ $seguro->patente }}" name="patente">
+                </div>
             </div>
         </div>
     </form>
