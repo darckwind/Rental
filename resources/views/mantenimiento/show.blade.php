4ec03cf8cd9f2e4275f90f626e3879bdcd1b5472
franciscolagos <f.bunout01@ufromail.cl>
2019-07-05 14:13:22 -0400

    cambio en mantenciones

--- resources/views/mantenimiento/show.blade.php
+++ resources/views/mantenimiento/show.blade.php
@@ -4,6 +4,6 @@
     @if($manteniento[0] == null)
         <p>no se precentan mantenciones para el vehiculo</p>
     @else
-        <p>{{$manteniento}}</p>
+        <p>{{$manteniento[0]}}</p>
     @endif
 @endsection
