7682d834539990bc99cdad1b0ebb43a91c6caa58
Riko <f.bunout01@ufromail.cl>
2019-07-05 01:08:13 -0400

    me olvide de agregar a fillable la fecha por eso no actualizaba

--- app/Seguro.php
+++ app/Seguro.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Seguro extends Model
 {
     protected $fillable = [
-        'patente','poliza','img_seg'
+        'patente','poliza','img_seg','venc_seg'
     ];
     protected $primaryKey = 'patente';
     public $incrementing = false;
