e6767b157f9405e664fcc11ff72846e0a20827e9
Riko <f.bunout01@ufromail.cl>
2019-07-05 11:06:17 -0400

    modificacion rent

--- resources/views/rent/create.blade.php
+++ resources/views/rent/create.blade.php
@@ -59,10 +59,13 @@
 			<div class="col-sm-3">
 				<input type="text" name="rent_to" class="form-control">
 			</div>
+
 		</div>
 		<div class="form-group row">
-			<label class="col-sm-2 col-form-label">Tiene hotel:</label>
-			<input type="checkbox" name="hotel" class="custom-control-input" id="defaultUnchecked">
+			<div class="form-group">
+				<input type="checkbox" name="hotel" class="custom-control-input" id="defaultUnchecked">
+				<label class="custom-control-label" for="defaultUnchecked">Has Hotel</label>
+			</div>
 		</div>
 		<button type="submit" class="btn btn-block btn-primary">Submit</button>
 	</form>
