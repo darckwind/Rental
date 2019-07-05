e4209426e391685e60ff2d34544f19d19f64964c
Riko <f.bunout01@ufromail.cl>
2019-07-04 20:20:40 -0400

    se modifica ruas de navbar

--- database/migrations/2019_05_20_215546_create_mantenientos_table.php
+++ database/migrations/2019_05_20_215546_create_mantenientos_table.php
@@ -16,19 +16,19 @@ class CreateMantenientosTable extends Migration
         Schema::create('mantenientos', function (Blueprint $table) {
             $table->string('patente', 100);
             $table->foreign('patente')->references('patente')->on('motos')->onDelete('cascade');
-	    $table->integer('kilometraje');
-	    $table->string('ac_lvl_original');
-	    $table->string('cadena_status');
-	    $table->string('refrigerante_lvl');
-	    $table->string('brake_lvl');
-	    $table->string('front_brake_status');
-	    $table->string('rear_brake_status');
-	    $table->string('batery_status');
-	    $table->string('12v_outlet');
-	    $table->string('puños_calefactables_status');
-	    $table->string('fuel_status');
-	    $table->string('luces_status');
-	    $table->string('notas_adicionales');
+            $table->integer('kilometraje');
+            $table->string('ac_lvl_original');
+            $table->string('cadena_status');
+            $table->string('refrigerante_lvl');
+            $table->string('brake_lvl');
+            $table->string('front_brake_status');
+            $table->string('rear_brake_status');
+            $table->string('batery_status');
+            $table->string('12v_outlet');
+            $table->string('puños_calefactables_status');
+            $table->string('fuel_status');
+            $table->string('luces_status');
+            $table->string('notas_adicionales');
             $table->timestamps();
         });
     }
