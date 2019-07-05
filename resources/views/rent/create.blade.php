69bd63be5961ec2453f66746d708c2e6c9591fb1
Riko <f.bunout01@ufromail.cl>
2019-07-05 11:10:32 -0400

    modificacion rent

--- resources/views/rent/create.blade.php
+++ resources/views/rent/create.blade.php
@@ -63,7 +63,7 @@
 		</div>
 		<div class="form-group row">
 			<input type="checkbox" name="hotel" class="custom-control-input" id="defaultUnchecked">
-			<label class="col-sm-2 col-form-label" for="defaultUnchecked">Has Hotel</label>
+			<label class="col-form-label" for="defaultUnchecked">Has Hotel</label>
 		</div>
 		<button type="submit" class="btn btn-block btn-primary">Submit</button>
 	</form>
