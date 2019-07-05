f54f000761669fc34cf89e2bd406924adfbebb1f
Riko <f.bunout01@ufromail.cl>
2019-07-04 23:51:59 -0400

    crud Seguro

--- resources/views/seguros/edit.blade.php
+++ resources/views/seguros/edit.blade.php
@@ -33,17 +33,15 @@
         <p>{{$seguro}}</p>
         <div class="form-group row">
             <label class="col-sm-2 col-form-label">Patente</label>
-            <div class="col-sm-4">
+            <div class="col-sm-2">
                 <input type="text" readonly class="form-control-plaintext" value="{{ $seguro->patente }}" name="patente">
             </div>
             <label class="col-sm-2 col-form-label">Poliza</label>
-            <div class="col-sm-4">
+            <div class="col-sm-2">
                 <input type="text" class="form-control" value="{{ $seguro->poliza }}" name="poliza">
             </div>
-        </div>
-        <div class="form-group row">
             <label class="col-sm-2 col-form-label">Vencimiento Seguro</label>
-            <div class="col-sm-4">
+            <div class="col-sm-2">
                 <input type="date" class="form-control" value="{{ $seguro->venc_seg }}" name="venc_seg">
             </div>
         </div>
